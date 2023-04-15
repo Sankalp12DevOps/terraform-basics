0eaa3e21e926e4f4789f4f203a28256e03ec621a
Sankalp12DevOps <sravanthi388@gmail.com>
2023-04-15 22:22:13 +0530

    m

---
+++ examples/ec2.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+ region = "us-east-1"
+}
+
+resource "aws_instance" "web" {
+  ami           = ami-0634358bf031cc921
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "INstance-1"
+  }
+}

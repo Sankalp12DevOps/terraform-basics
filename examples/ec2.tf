692bfd2fe9c7ddc4c87d2d990845448f6c0c377d
Sankalp12DevOps <sravanthi388@gmail.com>
2023-04-16 08:34:39 +0530

    m

--- examples/ec2.tf
+++ examples/ec2.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "web" {
   ami           = "ami-0634358bf031cc921"
   instance_type = "t3.micro"
-   vpc_security_group_ids = [aws_security_group.allow_ssh]
+   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
 
   tags = {
     Name = "INstance-1"

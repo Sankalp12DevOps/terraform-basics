55134b83d6cc35dcc7c4a302e3ee9efb2dbbcfbd
Sankalp12DevOps <sravanthi388@gmail.com>
2023-04-22 13:26:42 +0530

    m

--- examples-01/backend.tf
+++ examples-01/backend.tf
@@ -3,10 +3,10 @@ provider "aws" {
 }
 
 
-terraform {
-  backend "s3" {
-    bucket = "myterraform-s3-learning"
-    key    = "dev/terraform/tfstate"
-    region = "ap-south-1"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket = "myterraform-s3-learning"
+#     key    = "dev/terraform/tfstate"
+#     region = "ap-south-1"
+#   }
+# }
